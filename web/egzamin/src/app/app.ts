b248d5790ab1336fee2788fe8315835181422642
Mariusz <m.gogola@lukasiewicz-gorlice.pl>
2026-09-03 15:03:40 +0200

    m4

---
+++ web/egzamin/src/app/app.ts
@@ -0,0 +1,37 @@
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  standalone: true,
+  selector: 'app-root',
+  imports: [FormsModule],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+
+  adres: string = '';
+  login: string = '';
+  haslo: string = '';
+  wygeneruj: boolean = true;
+
+  adres_widok: string = '';
+  login_widok: string = '';
+  haslo_widok: string = 'automatycznie generowane';
+
+
+
+  zapisz() {
+
+    this.adres_widok =  this.adres;
+    this.login_widok  = this.login;
+    
+    if(this.wygeneruj==true){
+                            this.haslo_widok='automatycznie generowane';
+                            }
+                            else{
+                                  this.haslo_widok = this.haslo;
+                                }
+  }
+
+}
