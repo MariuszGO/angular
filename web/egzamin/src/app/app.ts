76e5783c045ccee25cca1a6bd0e4e14374c0deea
Mariusz <m.gogola@lukasiewicz-gorlice.pl>
2026-09-07 12:59:24 +0200

    m5

--- web/egzamin/src/app/app.ts
+++ web/egzamin/src/app/app.ts
@@ -13,24 +13,30 @@ export class App {
   adres: string = '';
   login: string = '';
   haslo: string = '';
+  haslo_form: string = '';
   wygeneruj: boolean = true;
 
   adres_widok: string = '';
   login_widok: string = '';
   haslo_widok: string = '';
 
+  litery_duze: string[]= ["A","B","C","D","E","F"];
+  litery_male: string[]= ["a","b","c","d","e","f"];
+  litera: number = 1;
+  
 
-
+ 
   zapisz() {
-
+    this.losuj();
     this.adres_widok =  this.adres;
     this.login_widok  = this.login;
+
     
     if(this.wygeneruj==true){
-                            this.haslo_widok='automatycznie generowane';
+                            this.haslo_widok= this.haslo;
                             }
                             else{
-                                  this.haslo_widok = this.haslo;
+                                  this.haslo_widok = this.haslo_form;
                                 }
   }
 
@@ -43,5 +49,33 @@ export class App {
     this.login_widok = '';
     this.haslo_widok = '';
   }
+
+  losuj(){
+    this.haslo ="";
+    let litera;
+    for(let i=0 ; i<3 ; i++){
+                            litera = Math.floor(Math.random()*3);
+                            this.haslo = this.haslo + this.litery_duze[litera];
+                            }
+
+    for(let i=0 ; i<3 ; i++){
+                            litera = Math.floor(Math.random()*3);
+                            this.haslo = this.haslo + this.litery_male[litera];
+                            }
+
+    let tablica = this.haslo.split('');
+    let tmp = '';
+    for(let i=0 ; i<6; i++){
+                            litera = Math.floor(Math.random()*7);
+
+                            tmp = tablica[litera];
+                            tablica[litera] = tablica[i];
+                            tablica[i] = tmp;
+                          }
+    this.haslo = tablica.join('');
+
+    console.log(this.haslo);
+  
+}
 
 }
